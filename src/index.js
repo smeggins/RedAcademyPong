ce31947b6897eb8af2c4a45d9b418cd4e98b3c50
smeggins <phillipchadwick@hotmail.com>
2019-10-23 20:54:15 -0700

    comment out svg code in HTML and create <svg> element and it's attributes in js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import Game from './partials/Game';
 
 // create a game instance
 const game = new Game('game', 512, 256);
+let svg = document.createElementNS (SVG_NS, "svg");
+svg.setAttributeNS(null, "width", this.width);
+svg.setAttributeNS(null, "height", this.height);
+svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+this.SVGAnimateElement.appendChild(svg);
 
 (function gameLoop() {
   game.render();
