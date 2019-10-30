0a1a68c3cc15c6925b1ce273267db02c1991f58f
smeggins <phillipchadwick@hotmail.com>
2019-10-30 13:57:44 -0700

    Generates js files for individual function groupings of pong project. defines game element in game class so that it can be referred to by the render methods append child property

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import Game from './partials/Game';
 
 // create a game instance
 const game = new Game('game', 512, 256);
-let svg = document.createElementNS (SVG_NS, "svg");
-svg.setAttributeNS(null, "width", this.width);
-svg.setAttributeNS(null, "height", this.height);
-svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
-this.SVGAnimateElement.appendChild(svg);
 
-(function gameLoop() {
+let lastTime = 0
+
+(function gameLoop(timeStamp) {
+  let dt = timeStamp - lastTime;
+  lastTime = timeStamp;
+  console.log(timeStamp, lastTime, dt);
+
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+  playerPaddle.update(dt);
+  playerPaddle.draw(ctx);
   game.render();
   requestAnimationFrame(gameLoop);
 })();
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,24 @@
+import SVG_NS from '../settings'
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
+    this.gameElement = document.getElementById('game')
 
-		// Other code goes here...
+
   }
 
   render() {
-		// More code goes here....
+
+    let svg = document.createElementNS(SVG_NS, "svg");
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(svg);
   }
-}
+
+
+};
+
